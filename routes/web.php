6ad57f0d298458359d4b7bee4e9ef5ce5ad07515
suhail-codenia <suhail.usmani@corewebconnections.com>
2024-10-09 17:22:53 +0530

    first commit

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('site.home');
+});
+
+Route::get('/about', function () {
+    return 'This is about page.';
 });
