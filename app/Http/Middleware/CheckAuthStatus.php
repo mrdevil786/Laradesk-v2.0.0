fd472dd12943eb105e56258749513bbd9504ebe7
suhail-codenia <suhail.usmani@corewebconnections.com>
2024-10-09 17:29:06 +0530

    first commit

---
+++ app/Http/Middleware/CheckAuthStatus.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAuthStatus
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = Auth::user();
+
+        if ($user && $user->status === 'blocked') {
+            Auth::guard('web')->logout();
+            return redirect()->route('admin.view.login')->with('warning', 'Your account is blocked.');
+        }
+
+        return $next($request);
+    }
+}
